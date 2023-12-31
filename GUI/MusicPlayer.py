af301655146898b9937aa98a3c1c224cdf8d7067
omardoescode <omardoescode@gmail.com>
2023-12-31 18:30:26 +0200

    Added save_game.py

--- GUI/MusicPlayer.py
+++ GUI/MusicPlayer.py
@@ -6,14 +6,19 @@ class MusicPlayer:
     
 
     # string -> ()
-    # An abstract function for running any music infinitely, given its path
-    def play_music(self,path):
+    # An abstract function for running any music infinitely, given its path, and the number of loops
+    # -1 means infinitely
+    def play_music(self,path, loops):
         pygame.mixer.music.load(path)
-        pygame.mixer.music.play(-1, 0)
+        pygame.mixer.music.play(loops, 0)
     
-    # Play the music
+    # Play the background music
     # TODO: Change these paths to the proper paths once created
-    play_default_music = lambda self: self.play_music('./assets/music/default.mp3')
-    play_winning_music = lambda self: self.play_music('./assets/music/default.mp3')
-    play_losing_music = lambda self: self.play_music('./assets/music/default.mp3')
-    play_board_music = lambda self: self.play_music('./assets/music/default.mp3')
+    play_default_music = lambda self: self.play_music('./assets/music/default.mp3', -1)
+    play_winning_music = lambda self: self.play_music('./assets/music/default.mp3', -1)
+    play_losing_music = lambda self: self.play_music('./assets/music/default.mp3', -1)
+    play_board_music = lambda self: self.play_music('./assets/music/default.mp3', -1)
+
+    # Play the effects music
+    # TODO: Add the music for flagged, unflagging, digging, start of a zero_chain, and so on
+    # TODO: They all must be of loops=1
---
+++ GUI/save_and_continue.py
@@ -0,0 +1,38 @@
+'''
+Cell:
+    val: [0:8] or F or M (1 through 8 for number or f for flagged or m for mines)
+    is_flagged: boolean
+    is_covered: boolean
+
+Board: (listof Cell)
+
+GameSavedObject: 
+    rows: int
+    columns: int
+    mines: int
+    baord: Board
+'''
+
+# string, int, int, int, Board -> ()
+# create a new file in folder games called <username>.json
+# This file has a GameSavedObject
+def store_game(username, rows, columns, mines, board):
+    pass
+
+# string -> (listof GameSavedObject)
+# Retrieves the GameSavedObject of the given username
+# the file should be in <username>.json
+# Return an empty object if there's none
+def retrieve_game(username):
+    pass
+
+# string -> boolean
+# delete the json file named <username>.json in games folder
+# Return true if the file has been deleted, or false if the file doesn't exist (OPTIONAL)
+def delete_game(username):
+    pass
+
+    
+
+
+
